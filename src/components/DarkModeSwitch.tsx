598737624798f733220c619d37f5f70c7e192211
adnan007d <adnanmansuri54@gmail.com>
2023-07-22 22:55:16 +0530

    prettier

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -3,11 +3,12 @@ import { Switch } from "./ui/switch";
 
 export const DarkModeSwitch = () => {
   return (
-    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" className="flex items-center space-x-2">
-      <Switch
-        id="light-mode-toggle"
-        checked={false}
-      />
+    <a
+      href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      target="_blank"
+      className="flex items-center space-x-2"
+    >
+      <Switch id="light-mode-toggle" checked={false} />
       <Label htmlFor="light-mode-toggle">Light Mode</Label>
     </a>
   );
