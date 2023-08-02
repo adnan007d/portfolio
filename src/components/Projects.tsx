bb6a38b603085002b77da992f5b26277bb14da9d
adnan007d <adnanmansuri54@gmail.com>
2023-08-02 11:32:35 +0530

    making mdx work bitch

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Optiminastic, {meta} from "@/projects/optiminastic.mdx";
 import fs from "fs";
 
 type Project = {
@@ -10,26 +9,26 @@ type Project = {
   otherLinks?: { name: string; url: string };
 };
 
-// async function getAllProjects(): Promise<Project[]> {
-//
-//   /*
-//    * Maybe change this to a raw for loop is there are performance issues later
-//    */
-//   const files = fs.readdirSync("src/projects/").filter(file => file.endsWith(".mdx"));
-//
-//   console.log(files);
-//
-//   // const metas: Project[] = await Promise.all(files.map(file => import(`@/projects/${file}`)));
-//   // console.log(metas)
-//   //
-//   // return metas
-// }
+async function getAllProjects(): Promise<Project[]> {
+  /*
+   * Maybe change this to a raw for loop is there are performance issues later
+   */
+  const files = fs
+    .readdirSync("src/projects/")
+    .filter((file) => file.endsWith(".mdx"));
 
-const Projects = () => {
-  // const projects = getAllProjects();
-  // console.log(projects);
-  console.log(meta)
+  const metas: Project[] = await Promise.all(
+    files.map((file) =>
+      import(`@/projects/${file}`).then((module) => module.meta)
+    )
+  );
 
+  return metas;
+}
+
+const Projects = async () => {
+  const projects = await getAllProjects();
+  console.log(projects);
   return <div>Projects</div>;
 };
 
