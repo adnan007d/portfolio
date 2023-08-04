f7f457d7260b8e5df03ac92f70e68c2d9015af76
adnan007d <adnanmansuri54@gmail.com>
2023-08-04 14:58:15 +0530

    github repos

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   experimental : {
     mdxRs: true,
-  }
+  },
+images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'opengraph.githubassets.com',
+        port: '',
+        pathname: '/*/adnan007d/**',
+      },
+    ],
+  },
 }
 const withMDX = require('@next/mdx')()
 module.exports = withMDX(nextConfig)
--- src/components/LatestGitRepo.tsx
+++ src/components/LatestGitRepo.tsx
@@ -1,13 +1,35 @@
+import Image from "next/image";
 import React from "react";
 
 /**
  * @throws FetchError
  */
+async function getOgImageFromGithub(url: string): Promise<string | undefined> {
+  console.log(url);
+
+  const html = await fetch(url, { next: { revalidate: 60 * 60 * 24 } }).then(
+    (res) => res.text()
+  );
+
+  const match = html.match(/property="og:image" content="([^"]*)" \/>/);
+  return match?.[1];
+}
+
+/**
+ * @throws FetchError
+ */
 async function getLatestGitRepos(): Promise<any[]> {
-  const data = await fetch(
+  const repos = await fetch(
     "https://api.github.com/users/adnan007d/repos?sort=created&per_page=3",
-    { next: { revalidate: 60 } }
+    { next: { revalidate: 60 * 60 * 24 } }
   ).then((res) => res.json());
+
+  const data = await Promise.all(
+    repos.map(async (repo: any) => ({
+      ...repo,
+      ogImage: await getOgImageFromGithub(repo.html_url),
+    }))
+  );
 
   return data;
 }
@@ -16,10 +38,28 @@ const LatestGitRepo = async () => {
   const repos = await getLatestGitRepos();
 
   return (
-    <div>
-      {repos.map((repo) => (
-        <span key={repo.id}>{repo.name} </span>
-      ))}
+    <div className="my-10">
+      <h4 className="text-4xl sm:text-5xl font-black text-center my-5 mx-2">
+        Latest Github Repositories
+      </h4>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center">
+        {repos.map((repo) => (
+          <a
+            href={repo.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-[350px] mx-3 max-w-[90vw] border-2 rounded-lg overflow-hidden border-secondary"
+            key={repo.id}
+          >
+            <Image
+              src={repo.ogImage}
+              width={1200}
+              height={600}
+              alt={`OG Image from ${repo.name}`}
+            />
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
