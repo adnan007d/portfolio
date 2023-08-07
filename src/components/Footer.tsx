b7db30a01ee39741fdccae78965d23b389710787
adnan007d <adnanmansuri54@gmail.com>
2023-08-07 23:23:47 +0530

    some svg and footer partial

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Icons } from "./ui";
 
 const Footer = () => {
-  return <div className="text-center">Footer</div>;
+  return (
+    <div className="flex flex-wrap gap-5 items-center justify-center py-5">
+      <Icons.github className="w-10 aspect-square" />
+      <Icons.mail className="w-10 aspect-square" />
+      <Icons.x className="w-8 aspect-square" />
+    </div>
+  );
 };
 
 export default Footer;
