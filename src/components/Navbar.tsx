135ca254f502803e47fd892457ca49965c85d9c5
adnan007d <adnanmansuri54@gmail.com>
2023-08-12 00:16:18 +0530

    Navbar redirect to home and meta data

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import React from "react";
 import { DarkModeSwitch } from "./DarkModeSwitch";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <header className="flex justify-between px-4 py-2">
-      <Image
-        src="/ket.webp"
-        width={48}
-        height={48}
-        alt="Profile Pic"
-        className="rounded-full"
-        priority
-      />
+      <Link href="/" aria-label="Navigate to Home">
+        <Image
+          src="/ket.webp"
+          width={48}
+          height={48}
+          alt="Profile Pic"
+          className="rounded-full"
+          priority
+        />
+      </Link>
       <DarkModeSwitch />
     </header>
   );
