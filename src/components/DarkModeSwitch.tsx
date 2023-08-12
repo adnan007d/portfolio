07325b3cc0aa1761130ba8b5e58d4236f59c8259
adnan007d <adnanmansuri54@gmail.com>
2023-08-12 23:59:48 +0530

    configs favicon and other small changes

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import { Label } from "./ui/label";
 import { Switch } from "./ui/switch";
 
 export const DarkModeSwitch = () => {
+  const onCheckedChanged = (checked: boolean) => {
+    if (checked) {
+      window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+    }
+  };
+
   return (
-    <a
-      href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-      target="_blank"
-      className="flex items-center space-x-2"
-    >
-      <Switch id="light-mode-toggle" checked={false} aria-label="light-mode-toggle"/>
+    <div className="flex items-center space-x-2">
+      <Switch
+        onCheckedChange={onCheckedChanged}
+        id="light-mode-toggle"
+        checked={false}
+      />
       <Label htmlFor="light-mode-toggle">Light Mode</Label>
-    </a>
+    </div>
   );
 };
